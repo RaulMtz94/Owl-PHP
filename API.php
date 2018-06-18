c20f290cdd574a1db545b217fa6cf8749112a6a4
unknown <martinezvalenzuelaraul@gmail.com>
2018-06-18 13:47:27 -0600

    commit

---
+++ API.php
@@ -0,0 +1,211 @@
+﻿<?php
+class Ontology{
+    public $ontology;
+            public function Ontology($url){
+                //Validar que la ruta exista
+                if(file_exists($url) === FALSE){
+                exit("El archivo ingresado: $url no existe");
+                }
+                    $fp = fopen($url,'r');
+                
+                //Validar que el archivo tenga un formato correcto
+                    $fread = fread($fp, filesize($url));
+                    
+                //Asignación del archivo a la variable $ontology
+                if($fread === TRUE){
+                    $this->ontology = $fread;
+                }
+
+                if($fread ===FALSE){
+                    exit("Falló la apertura del flujo al URL");
+                }
+                $this->ontology = $fread;
+            }
+            
+    //RENAME GetAllClass
+    function getClasses(){
+
+            if (strpos($this->ontology, 'owl:Class') !== false) {
+                $array_classes[]="";
+                preg_match_all('/:[a-zA-Z]+[a-zA-Z0-9]*\s(rdf:type owl:Class)/', $this->ontology, $coincidencias);
+                for($i=0; $i<count($coincidencias[0]); $i++){ 
+                    //Quitar rdf:typ... dejar sólo la clase y agregarla al arreglo
+                    $array_classes[$i] = trim(substr($coincidencias[0][$i], 0, strpos($coincidencias[0][$i], "rdf:type owl:Class")),":");
+                }
+                return $array_classes;
+            }else{
+                //Retorna nullo si la ontología no tiene clases
+                return null;
+            }
+        } 
+
+
+
+        function getClass($class){
+
+            if (strpos($this->ontology, 'owl:ObjectProperty') !== false) {
+                $datos = null;
+                $rango="";
+                $nombre="";
+                $dominio="";
+                $tipo="";
+                preg_match_all('/:[a-zA-Z]+[a-zA-Z0-9]*\s(rdf:type owl:ObjectProperty ;)[\sa-z:;.]*/', $this->ontology, $coincidencias);
+                for($i=0; $i<count($coincidencias[0]); $i++){
+                    $clase = trim($this->sustituir("rdf:type owl:ObjectProperty ;",",", $coincidencias[0][$i]),":");
+                    $clase = $this->sustituir("rdfs:","",$clase);
+                    $clase = $this->sustituir(";",",",$clase);
+                    $clase1=$clase;
+                    $existe = strpos($clase,"range :".$class);
+                    
+                   
+                    $existe2 = strpos($clase,"domain :".$class);
+                    
+                    if ($existe==true){
+                        $clase = $this->sustituir("domain :","",$clase1);
+                        $clase = $this->sustituir("range xsd:","",$clase1); 
+                        $clase = $this->sustituir(".","",$clase1); 
+                        $clase = $this->sustituir(" ","",$clase1);  
+                        $clase = $this->sustituir("\n","",$clase1);  
+                        list($name2, $class2, $datatype2) = explode(",",$clase);
+                        $rango = $rango. $name2;
+                    }
+                    if ($existe2==true){
+                        $clase = $this->sustituir("domain :","",$clase1);
+                        $clase = $this->sustituir("range xsd:","",$clase1); 
+                        $clase = $this->sustituir(".","",$clase1); 
+                        $clase = $this->sustituir(" ","",$clase1);  
+                        $clase = $this->sustituir("\n","",$clase1);  
+                        list($name, $class, $datatype) = explode(",",$clase);
+                        $dominio = $dominio. $name;
+                    }
+                   
+
+                }
+                preg_match_all('/:[a-zA-Z]+[a-zA-Z0-9]*\s(rdf:type owl:DatatypeProperty ;)[\sa-zA-ZñÑáéíóúÁÉÍÓÚ:;.]*/', $this->ontology, $coincidencias); 
+                for($i=0; $i<count($coincidencias[0]); $i++){
+                    $clase = trim($this->sustituir("rdf:type owl:DatatypeProperty ;",",", $coincidencias[0][$i]),":");
+                    $clase = $this->sustituir("rdfs:","",$clase);
+                    $clase = $this->sustituir(";",",",$clase);
+                    $clasebuscar= explode(":",$class);
+                    
+                    $existe3 = strpos($clase,"domain :".trim($clasebuscar[1]));
+                    if($existe3==true){
+                       
+                        $clase = $this->sustituir("domain :","",$clase);
+                        $clase = $this->sustituir("range xsd:","",$clase); 
+                        $clase = $this->sustituir(".","",$clase); 
+                        $clase = $this->sustituir(" ","",$clase);  
+                        $clase = $this->sustituir("\n","",$clase);  
+                        list($name3, $class3, $datatype3) = explode(",",$clase);
+                        $nombre =  $name3;
+                        $tipo = $datatype3;
+                    }
+                }
+                $datos = array(
+                    "nombre" => $nombre,
+                    "tipo" => $tipo,
+                    "rango" => $rango,
+                    "dominio" =>  $dominio
+                ); 
+ 
+                return $datos;
+            } 
+            return null;
+        }
+        function getDataProperties($class){
+        
+            if (strpos($this->ontology, 'owl:DatatypeProperty') !== false) { 
+                preg_match_all('/:[a-zA-Z]+[a-zA-Z0-9]*\s(rdf:type owl:DatatypeProperty ;)[\sa-zA-ZñÑáéíóúÁÉÍÓÚ:;.]*/', $this->ontology, $coincidencias); 
+                for($i=0; $i<count($coincidencias[0]); $i++){
+                    $clase = trim($this->sustituir("rdf:type owl:DatatypeProperty ;",",", $coincidencias[0][$i]),":");
+                    $clase = $this->sustituir("rdfs:","",$clase);
+                    $clase = $this->sustituir(";",",",$clase);
+                    $existe = strpos($clase,"domain :".$class);
+                    if($existe==true){
+                        $clase = $this->sustituir("domain :","",$clase);
+                        $clase = $this->sustituir("range xsd:","",$clase); 
+                        $clase = $this->sustituir(".","",$clase); 
+                        $clase = $this->sustituir(" ","",$clase);  
+                        $clase = $this->sustituir("\n","",$clase);  
+                        list($name, $class, $datatype) = explode(",",$clase);
+                        $datos = array(
+                            "name" => $name,
+                            "domain" =>  $class,
+                            "range" => $datatype
+                        ); 
+                    }
+                }
+                return $datos;
+            } 
+            return null;
+        }
+        function getAllIndividuals(){
+            if (strpos($this->ontology, 'owl:NamedIndividual') !== false) {
+                $array_individual = array();
+                preg_match_all('/:[a-zA-Z]+[a-zA-Z0-9]*\s(rdf:type owl:NamedIndividual ,)+[\sa-z0-9A-ZñÑáéíóúÁÉÍÓÚ"^:;.]*/', $this->ontology, $coincidencias);
+                for($i=0; $i<count($coincidencias[0]); $i++){
+                    $clase = trim($this->sustituir("rdf:type owl:DatatypeProperty ;",",", $coincidencias[0][$i]),":");
+                    $clase = $this->sustituir("rdfs:","",$clase);
+                    $clase = $this->sustituir("range xsd:",",tipo:",$clase);
+                    $clase = $this->sustituir(";","",$clase);
+                    $array_individual[$i]= $clase;
+                }
+            } 
+            return $array_individual;
+        }
+        function getIndividual($individual){
+            if (strpos($this->ontology, 'owl:NamedIndividual') !== false) {
+                preg_match_all('/:[a-zA-Z]+[a-zA-Z0-9]*\s(rdf:type owl:NamedIndividual ,)+[\sa-z0-9A-ZñÑáéíóúÁÉÍÓÚ"^:;.]*/', $this->ontology, $coincidencias);
+                for($i=0; $i<count($coincidencias[0]); $i++){
+                    $clase = trim($this->sustituir("rdf:type owl:DatatypeProperty ;",",", $coincidencias[0][$i]),":");
+                    $clase = $this->sustituir("rdfs:","",$clase);
+                    $clase = $this->sustituir("range xsd:",",tipo:",$clase); 
+                    $clase = $this->sustituir("\n"," ", $clase); 
+                    //echo "$clase<br><br><br>";
+                    $temporal = array(); 
+                    $temporal = explode(";", $clase); 
+                    if(strpos(" ".$temporal[0], $individual)){
+                        $name = $this->sustituir(" rdf:type owl:NamedIndividual ,","", $temporal[0]);
+                        $name = $this->sustituir(" ","", $name);
+                        list($valor, $name) = explode(":", $name);
+
+                        $datos = array(
+                            $name => $valor
+                        ); 
+
+                        for($i=1; $i<count($temporal)-1; $i++){
+                            $item = trim($this->sustituir("             ", "", $temporal[$i])); 
+                            $item = $this->sustituir(":", "", $item); 
+                            list($clave, $valor) = explode(" ", $item);
+                            $datos[$clave] = $valor;
+                        }
+                        $ultimoItem = $temporal[count($temporal)-1];
+                        $ultimoItem = trim($this->sustituir("             ", "", $ultimoItem)); 
+
+                        if(strpos($ultimoItem, "\"")){
+                            list($clave, $valor, $datatype) = explode("\"",$ultimoItem);
+                        }else{
+                            list($clave, $valor) = explode(" ",$ultimoItem); 
+                        }
+                        
+                        $clave = $this->sustituir(":","", $clave);
+                        $datos[$clave] = $valor;
+                        
+                        return $datos;
+                    } 
+                }
+            } 
+           
+        }
+
+        // Métodos para manipulación de cadenas para uso de la propia API.
+        function before($t, $inthat){
+            return substr($inthat, 0, strpos($inthat, $t));
+        }
+        function sustituir($original,$sustituir,$cadena){
+            return str_replace($original, $sustituir, $cadena);
+        }
+    } 
+?>
+
+
---
+++ usuario.php
@@ -0,0 +1,68 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include "./API.php"?>
+    <title>Probando API ontología en PHP</title>
+</head>
+<body>
+    <?php
+        $ontologia = new Ontology("carreras.owl");
+        //$ontologia->cargarOntologia("carrera.owl");
+        echo "############################## CLASES ################################<br>";
+        print_r($ontologia->getClasses());
+        echo "<br><br>########################## PROPIEDADES DE CLASSE ###########################<br>";
+        echo "<br>PRUEBA CON CARRERA<br>";
+        print_r($ontologia->getClass("carrera"));
+        echo "<br>PRUEBA CON MATERIA<br>";
+        print_r($ontologia->getClass("materia"));
+        echo "<br>############################## INDIVIDUOS ################################<br>";
+        print_r($ontologia->getAllIndividuals());
+       
+
+        echo "<br><br>################### INDIVIDUALS ESTUDIANTE1 ##########################<br>";
+        print_r($ontologia->getIndividual("estudiante1"));
+        echo "<br><br>##################### INDIVIDUALS CARRERA1 ###########################<br>";
+        print_r($ontologia->getIndividual("carrera1")); 
+        echo "<br><br>###################### INDIVIDUALS GRUPO1#######################<br>";
+        print_r($ontologia->getIndividual("grupo1")); 
+        echo "<br><br>######################### PRUEBA FINAL #########################<br>";
+        
+        //El arreglo $estudiante1 recibirá las propiedades del individuo "estudiante1"
+        $estudiante1 = $ontologia->getIndividual("estudiante1");
+        print_r($estudiante1);
+        
+        $materia1 = $ontologia->getIndividual("materia1");
+        $carrera1 = $ontologia->getIndividual("carrera1");
+        $grupo1 = $ontologia->getIndividual("grupo1");
+/*
+        // Imprimir todos los campos del individuo
+        foreach($estudiante1 as $campo){
+            $clave = array_keys($estudiante1, $campo); 
+            echo "$clave[0] = $campo <br>";
+        }
+*/
+        //Imprimir los campos con un "Join"
+        echo "estudiante: ".$estudiante1['estudiante'];
+        echo "<br>nombre: ".$estudiante1['nombreEstudiante '];
+        echo "<br>cursa: ".$materia1['nombreMateria '];
+        echo "<br>pertenece: ".$grupo1['grupoID'];
+    
+        $nombre="name";
+        $texto="asdas";
+
+        $lins = file("carreras.owl");
+
+        $LINE_TO_WRITE = count($lins);
+        $lins[126] = "Texto desde PHP";
+        
+        // Escribimos de nuevo el texto en el archivo (versión BRUTUS)
+        $fh = fopen("carreras.owl", 'wt'); //** Atención en el modo
+        fwrite($fh, implode('', $lins));
+        fclose($fh);
+
+    ?>
+</body>
+</html>
